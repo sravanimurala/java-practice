51a07b5ab5b4bbb8cbb29bd528c20e35c345ad92
sravanimurala <sravanimurala5@gmail.com>
2021-07-07 14:06:43 +0530

    second commit

--- src/Welcome.java
+++ src/Welcome.java
@@ -3,6 +3,7 @@ public class Welcome {
 	public static void main(String[] args) {
 System.out.println("Welcome to java");
 	System.out.println("Welcome here");	
+	System.out.println("java is the secure language");
 	}
 	
 
